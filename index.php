b093a9fc9401d41e412faa8afa9eeb08d01c229d
Taity-mini <taitymini@hotmail.co.uk>
2015-11-27 12:58:17 +0000

    Link to database and show all table names

--- index.php
+++ index.php
@@ -11,10 +11,6 @@ include("connect.php");
 
 echo "Home Page";
 
-$result = mysql_query("show tables"); // run the query and assign the result to $result
-while($table = mysql_fetch_array($result)) { // go through each row that was returned in $result
-    echo($table[0] . "<BR>");    // print the table that was returned on that row.
-}
-
+$listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0);
 
 ?>

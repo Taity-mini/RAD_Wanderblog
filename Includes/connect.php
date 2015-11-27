31a113ac43fcc6f7d4270f16c9969e7b0fe732e3
Taity-mini <taitymini@hotmail.co.uk>
2015-11-27 12:54:26 +0000

    Link to database and show all table names

--- Includes/connect.php
+++ Includes/connect.php
@@ -1,7 +1,22 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Andrew
+ * User: RAD
  * Date: 26/11/2015
  * Time: 16:40
+ *
+ * Description: Connect to Database Script
+ * 
  */
+
+$db = new mysqli(
+    "eu-cdbr-azure-north-d.cloudapp.net", //db host
+    "b49912ac2cf930", //username
+    "e632d092", //pw
+    "RGU_1504693"  //db_name
+
+);
+
+if(!$db) {
+    die('Connect Error:' .mysqli_connect_errno());
+}
--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
-/**
+/*
  * Created by PhpStorm.
  * User: RAD
  * Date: 26/11/2015
  * Time: 16:40
+ * Description: Main index page for WanderBlog
  */
 
+include("connect.php");
+
 echo "Home Page";
+
+$result = mysql_query("show tables"); // run the query and assign the result to $result
+while($table = mysql_fetch_array($result)) { // go through each row that was returned in $result
+    echo($table[0] . "<BR>");    // print the table that was returned on that row.
+}
 
 
 ?>

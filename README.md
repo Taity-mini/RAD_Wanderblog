2730cb5383df40033d7642ae03314bf7397db435
Taity-mini <taitymini@hotmail.co.uk>
2015-12-07 15:52:12 +0000

    Create website template layout (HTML + CSS)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # RAD_Wanderblog
 Group Project by RAD for CM3028 Web Applications Development
-Website Address:
+#Website Address:
 http://rad-cm3028.azurewebsites.net/
--- index.php
+++ index.php
@@ -7,11 +7,54 @@
  * Description: Main index page for WanderBlog
  */
 
-include("includes/connect.php");
+//include once("includes/connect.php");
 
-echo "Home Page";
 
-$listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0);
+//$listdbtables = array_column(mysqli_fetch_all($db->query('SHOW TABLES')),0);
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Wanderblog | Home Page</title>
+    <link rel="stylesheet" href="./style.css" />
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div id="Wrapper">
+    <div id="Header">
+        <p>Header image goes here!</p>
+        <div class ="nav">
+            <ul>
+                <li class ="Home"><a>Home</a></li>
+                <li class ="Login"><a>Login</a></li>
+                <li class ="Register"><a>Register</a></li>
+            </ul>
+        </div>
+    </div>
+    <div id ="Content">
+        <h1>Header</h1>
+        <div id ="Content-inner">
 
 
+            <p>Hello</p<br />
+            <br />
+            <ol>
+                <li></li>
+                <li></li>
+                <li>&nbsp;&nbsp;</li>
+            </ol>
+            <br />
+        </div>
+        <br />
+    </div>
+</div>
+
+    <div id ="Footer">
+        <p>Footer</p>
+    </div>
+</div>
+</body>
+</html>

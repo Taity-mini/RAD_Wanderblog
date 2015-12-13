a0597306abf804be7df90afb50778b64d0a9c7de
zazzy <zazzy-loves-games@hotmail.co.uk>
2015-12-13 19:15:22 +0000

    yolo
    swag

--- welcome.php
+++ welcome.php
@@ -7,18 +7,61 @@
  */
 
 ?>
+
+<head>
+
+<title>
+	Home 
+</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div id = "Welcome_Inner_Wrap">
 <h1><?php echo $content_header ?></h1>
+<h2> </h2>
+
 <div id ="Content-inner">
 
 
-    <p>Content Goes here!</p<br /
-    <br />
-    <p><?php // echo $_SESSION["groupID"]; ?></p>
-    <ol>
-        <li></li>
-        <li></li>
-        <li>&nbsp;&nbsp;</li>
-    </ol>
+	<div id = "Welcome-Content-blob">
+		<div class = "Scroll_Banner">
+		<img class = "Welcome_Picture_Container"/>
+		<img src="temp.jpg" alt="Coffee Shop" >
+		<img src="temp.jpg" alt="Coffee Shop" >
+		<img src="temp.jpg" alt="Coffee Shop" >
+   		</div>
+    </div>
     <br />
 </div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+</div>
+<div id = "Welcome_Outter_Wrap">
+<h1>Trending</h1>
+<h2> </h2>
+	<div id = "Content-outter">
+	<div class = "Trending_Picture_Container"><header><h4>Votes</h4></header></div>
+	<div class = "Trending_Picture_Container"><header><h4>Votes</h4></header></div>
+	</div>
+
+</div>
+
+
 <br />
+
+</body>
+
+
+
+</html>

25a5a627c61aae94756992f45971858d96234bb3
zazzy <zazzy-loves-games@hotmail.co.uk>
2015-12-14 14:23:49 +0000

    TEst

--- Includes/page/view_adventure.php
+++ Includes/page/view_adventure.php
@@ -1,12 +1,18 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: RAD
- * Date: 13/12/2015
- * Time: 15:32
+ * User: Andrew
+ * Date: 08/12/2015
+ * Time: 11:53
  */
+
 ?>
+
 <head>
+
+<title>
+	Home 
+</title>
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
@@ -17,61 +23,45 @@
 
 <div id ="Content-inner">
 
-<div id = "columns">
-        <div id = "content-blob">
-           <div class = "Picture_Container">
-            <img src="temp.jpg" alt="Coffee Shop" >
-           </div>
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                <div id = "content-blob">
-           <div class = "Picture_Container">
-            <img src="temp.jpg" alt="Coffee Shop" >
-           </div>
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                <div id = "content-blob">
-           <div class = "Picture_Container">
-            <img src="temp.jpg" alt="Coffee Shop" >
-           </div>
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                <div id = "content-blob">
-           <div class = "Picture_Container">
-            <img src="temp.jpg" alt="Coffee Shop" >
-           </div>
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                <div id = "content-blob">
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                <div id = "content-blob">
-           <div class = "Picture_Container">
-            <img src="temp.jpg" alt="Coffee Shop" >
-           </div>
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                        <div id = "content-blob">
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-                        <div id = "content-blob">
-           <div class = "Text_Container">
-           Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb Rubarb 
-           </div>
-        </div>
-        </div>
-        </div>
+
+	<div id = "Welcome-Content-blob">
+		<div class = "Scroll_Banner">
+		<img class = "Welcome_Picture_Container"/>
+		<img src="temp.jpg" alt="Coffee Shop" >
+		<img src="temp.jpg" alt="Coffee Shop" >
+		<img src="temp.jpg" alt="Coffee Shop" >
+   		</div>
+    </div>
+    <br />
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+
+</div>
+<div id = "Welcome_Outter_Wrap">
+<h1>Trending</h1>
+<h2> </h2>
+	<div id = "Content-outter">
+	<div class = "Trending_Picture_Container"><header><h4>Votes</h4></header></div>
+	<div class = "Trending_Picture_Container"><header><h4>Votes</h4></header></div>
+	</div>
+
+</div>
+
+
+<br />
+
+</body>
+
+
+
+</html>

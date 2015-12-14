24dba91c4bec8f635553368b0f200dbb0e372963
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-14 19:27:59 +0000

    Update add_adventure.php

--- Includes/page/add_adventure.php
+++ Includes/page/add_adventure.php
@@ -144,14 +144,15 @@ if (!$error) {
             <td>Trip Date</td>
             <td><input maxlength="10" name="trip_Date" id="datepicker"  type="text" /></td>
         </tr>
+        <td>Trip Images</td>
+            <td><input type="file" name="fileToUpload" id="fileToUpload"></td>
+        </tr>
         <tr>
             <td><input name="submit" style="text-align: center" type="submit" value="Add Adventure" /></td>
             <td><input type="reset" value="Reset" /></td>
         </tr>
         <tr>
-            <td>Trip Images</td>
-            <td><input type="file" name="fileToUpload" id="fileToUpload"></td>
-        </tr>
+
 
     </table>
         <br/>

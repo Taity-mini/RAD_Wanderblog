ae1dc61b0ac09cc0a09a9585f19974363ee0c4b8
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-15 15:24:02 +0000

    Update add_adventure.php

--- Includes/page/add_adventure.php
+++ Includes/page/add_adventure.php
@@ -81,7 +81,7 @@ if (!$error) {
     else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
             echo "entered 1";
-            $insert = "INSERT INTO pages (title,trip_country, tags, userID, trip_Date, mod_Date)VALUES ('$title','$trip_country', $tags , '$userID',  '$trip_Date', '$mod_Date')";
+            $insert = "INSERT INTO pages (title,trip_country, tags, userID, trip_Date, mod_Date)VALUES ('$title','$trip_country', '$tags' , '$userID',  '$trip_Date', '$mod_Date')";
             $result = mysqli_query($db, $insert);
             if ($result) {
             echo "entered 2";

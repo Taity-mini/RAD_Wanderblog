7104b8733865f2b05dde88128b037e055f0ddae7
Taity-mini <taitymini@hotmail.co.uk>
2015-12-15 12:43:51 +0000

    Add test browser batch file

---
+++ Files/RAD_Browsers.bat
@@ -0,0 +1,10 @@
+@echo off
+ set var=rad-cm3028.azurewebsites.net
+ start "Chrome" "chrome" %var%
+ start "Internet Explorer" "C:\Program Files\Internet Explorer\IEXPLORE.EXE" %var%
+ start "Firefox" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" %var%
+ start "Opera" "C:\Program Files (x86)\Opera\launcher.exe" %var%
+ start "Edge(New IE)" "microsoft-edge" %var%
+ start "Chrome" "chrome" %var%
+	
+ exit

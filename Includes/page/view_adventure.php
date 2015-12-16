9d41e499da6c9d31a10c781ef14c2a8c4375883e
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-16 22:32:27 +0000

    Update view_adventure.php

--- Includes/page/view_adventure.php
+++ Includes/page/view_adventure.php
@@ -255,7 +255,7 @@ else
     <?php
     while($images1 = mysqli_fetch_array($pictures))){
     echo '<div id = "Small-Img">';
-    echo '<img id = "Big" src=".$images1['filePath'].">';
+    echo '<img id = "Big" src='".$images1['filePath']."'>';
     echo '</div>';
     }
     ?>	

934cfb03b5c9906b4aac27cc9be1422987c0b2c2
zazzy <zazzy-loves-games@hotmail.co.uk>
2015-12-16 16:03:35 +0000

    Layout++

--- Includes/user/view_user.php
+++ Includes/user/view_user.php
@@ -19,11 +19,13 @@
 
     	<div id = "Profile-Horizontal-Split-Left">
     	<div id ="Profile-Picture"><img src="/Res/temp4.jpg"></div>
+    	<div id ="Profile-bio"><h9>Bio</h9>Yo dawg my name's dave and i do stuff, things and other stuff.</div>
     	</div>
 
 
-    	<div id ="Information-Header"><h1>Person Name</h1><h2> </h2></div>
+    	<div id ="Information-Header"><h1>Dave DoStuff</h1><h2> </h2></div>
     	<div id ="Information-Content">
+    	<div id ="Adventure-Bio"><h9>Content Description</h9>My adventures consist of germany and elgin because i'm wiiiiild. God is love, Rev, Rob.</div>
     	  <div id ="Personal-Columns">
     		<div id = "Personal-Highest"><img src="/Res/temp4.jpg"></div>
     		<div id = "Personal-Highest"><img src="/Res/temp4.jpg"></div>
@@ -36,9 +38,9 @@
 
 
 
-    <div id = "Adventure-Profile-Content">
+    <div id = "Adventure-Profile-Content-0">
     </div>
-    <div id = "Adventure-Profile-Content">
+    <div id = "Adventure-Profile-Content-1">
     </div>
     <div id = "Adventure-Profile-Content">
     </div>

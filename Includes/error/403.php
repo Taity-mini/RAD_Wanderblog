4eff6dd74a7449e39089d11fe148d1c64c1f217c
Taity-mini <taitymini@hotmail.co.uk>
2015-12-17 00:35:25 +0000

    403 error page added (basic)

--- Includes/error/403.php
+++ Includes/error/403.php
@@ -7,3 +7,16 @@
  * Error 403 (Forbidden) page
  */
 ?>
+<h1><?php echo $content_header ?></h1>
+<div id ="Content-inner">
+
+
+    <p>Content Goes here!</p><br />
+    <br />
+    <ol>
+        <li></li>
+        <li></li>
+        <li>&nbsp;&nbsp;</li>
+    </ol>
+    <br />
+</div>

cc1c4b1e533d2b4a77665114768e1b149ddd9b07
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-17 16:15:25 +0000

    Update view_user.php

--- Includes/user/view_user.php
+++ Includes/user/view_user.php
@@ -11,7 +11,7 @@ $UserID1 = htmlentities($_GET['id']);
 
 $profileQuery = mysqli_query($db, "SELECT * FROM users WHERE userID = '$UserID1'");
 
-$pageDetails = mysqli_query($db, "SELECT * FROM pages WHERE userID = '$UserID1' ORDER BY PageID ASC limit 3" );
+$pageDetails = mysqli_query($db, "SELECT * FROM pages WHERE userID = '$UserID1' ORDER BY PageID ASC" );
 
 $getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID'");
 
@@ -32,18 +32,18 @@ $userDetails = mysqli_fetch_array($profileQuery);
 
     	<div id = "Profile-Horizontal-Split-Left">
     	<div id ="Profile-Picture"><img src="/Res/temp4.jpg"></div>
-    	<div id ="Profile-bio"><h3>Bio</h3>Yo dawg my name's dave and i do stuff, things and other stuff.</div>
+    	<div id ="Profile-bio"><h3>Bio</h3><?php echo $userDetails['bio']; ?></div>
     	</div>
 
 
-    	<div id ="Information-Header"><h1><?php echo $userDetails['first_Name'] . " " . $userDetails['last_Name']; ?></h1><h2></h2></div>
+    	<div id ="Information-Header"><h1><?php echo $userDetails['first_Name'] . " " . $userDetails['last_Name']; ?></h1></div>
     	<div id ="Information-Content">
-    	<div id ="Adventure-Bio"><h3>Content Description</h3>My adventures consist of germany and elgin because i'm wiiiiild. God is love, Rev, Rob.</div>
+    	<div id ="Adventure-Bio"><h3>My Trips</h3></div>
     	  <div id ="Personal-Columns">
     	    <?php
     	    while($pictures = mysqli_fetch_array($pageDetails)){
     	        $picsID = $pictures['PageID'];
-    	        $getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID'");
+    	        $getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID' limit 3");
     	        while($images = mysqli_fetch_array($getPics)){
         		    echo '<a href="./?page=adventure&id=' . $pictures['PageID'] . '"><div id = "Personal-Highest" ><img src="'.$images['filePath'].'" tabindex ="0" alt = "' . $pictures['title'] . '"></div></a>';
     	        }
@@ -58,68 +58,12 @@ $userDetails = mysqli_fetch_array($profileQuery);
 
 
     <div id = "Adventure-Profile-Content-0">
+    
     <div id = "Small-Img">
     <img id = "Big" src="./Res/temp2.jpg">
     </div>
 
-        <div id = "Small-Img">
-
-    <img id = "Big" src="./Res/temp3.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-
-    <img id = "Big" src="./Res/temp4.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-
-    <img id = "Big" src="./Res/temp1.jpg">
-    </div>
-
-        <div id = "Small-Img">
-
-    <img id = "Big" src="./Res/temp2.jpg">
-    </div>
-
-        <div id = "Small-Img">
-
-    <img id = "Big" src="./Res/temp3.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp4.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp1.jpg">
-    </div>
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp2.jpg">
-    </div>
-
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp3.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp4.jpg">
-    </div>
-
-
-        <div id = "Small-Img">
-    <img id = "Big" src="./Res/temp1.jpg">
-    </div>
-
-            <div id = "Small-Img">
-
-    </div>
-
+    
 
 
     </div>

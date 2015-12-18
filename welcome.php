1320d9d530b1b555f0477e4379990c7041859790
Taity-mini <taitymini@hotmail.co.uk>
2015-12-18 16:36:07 +0000

    Trending bar replaced divs with tables

--- welcome.php
+++ welcome.php
@@ -42,10 +42,27 @@ $vote = mysqli_query($db,"SELECT SUM(vote_Count) as count, pageID FROM `votes` g
 				}
 
 				?>
+				</br>
 			</div>
 		</div>
 	</div>
+	<?php
+				while($pictures = mysqli_fetch_array($vote)) {
+					$picsID = $pictures['pageID'];
+					$getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID'");
 
+					while ($images = mysqli_fetch_array($getPics)) {
+						if ($count == 1) {
+							echo '<a href="./?page=adventure&id=' . $pictures['pageID'] . '"><img class="Begining-img" src="' . $images['filePath'] . '" alt = "' . $pictures['title'] . '"></a>';
+						} else {
+							echo '<a href="./?page=adventure&id=' . $pictures['pageID'] . '"><img src="' . $images['filePath'] . '" alt = "' . $pictures['title'] . '"></a>';
+						}
+						$count++;
+					}
+				}
+
+				?>
+	?>
 </div>
 </div>
 </div>

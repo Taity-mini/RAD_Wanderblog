222052768f71232e880de499363f85ad72078993
Taity-mini <taitymini@hotmail.co.uk>
2015-12-18 00:54:03 +0000

    Added delete user and pages functions

--- Includes/delete.php
+++ Includes/delete.php
@@ -24,6 +24,31 @@ if (isset($type)) {
                     if($delete_exercises)
                     {
                         $msg = "Delete group successful";
+                        echo "<script> alert('Group Deleted Successfully');</script>";
+                        $page = "./?page=admin";
+                        header("Refresh: 2; URL=\"" . $page . "\"");
+                    }
+
+                } else
+                {
+                    echo "<script> alert('Delete Group Failed');</script>";
+                    $page = "./?page=admin";
+                    header("Refresh: 2; URL=\"" . $page . "\"");
+                }
+            break;
+
+        case "perm":
+
+                $perm_check = permissionInUse($db, $getid, "Check");
+                echo $perm_check;
+
+                if($perm_check)
+                {
+                    $delete_perm = mysqli_query($db,"DELETE FROM `group_permissions` WHERE permissionID = '$getid'");
+
+                    if($delete_perm)
+                    {
+                        $msg = "Delete group permissions successful";
                         echo "<script> alert('Group Added Successfully');</script>";
                         $page = "./?page=admin";
                         header("Refresh: 2; URL=\"" . $page . "\"");
@@ -31,25 +56,44 @@ if (isset($type)) {
 
                 } else
                 {
-                    echo "<script> alert('Delete Failed Failed');</script>";
+                    echo "<script> alert('Delete Permission Failed');</script>";
                     $page = "./?page=admin";
                     header("Refresh: 2; URL=\"" . $page . "\"");
                 }
             break;
+        //If user gets deleted so does all their content.
+        case "user":
 
-        case "perm":
-            function delete_permissions($getid)
-            {
-                //
-            }
+                $delete_user = mysqli_query($db,"DELETE FROM `user` WHERE userID = '$getid'");
+                $delete_user_pages = mysqli_query($db,"DELETE FROM `pages` WHERE userID = '$getid'");
+                $delete_user_comments = mysqli_query($db,"DELETE FROM `comments` WHERE userID = '$getid'");
+                $delete_user_votes = mysqli_query($db,"DELETE FROM `votes` WHERE userID = '$getid'");
+                $delete_user_pictures = mysqli_query($db,"DELETE FROM `picture_gallery_users` WHERE userID = '$getid'");
+
+
+
+                    echo "<script> alert('User Deleted Successfully');</script>";
+                    $page = "./?page=admin";
+                    header("Refresh: 2; URL=\"" . $page . "\"");
 
             break;
-        case "user":
-            function delete_user($getid)
-            {
-                //
 
-            }
+
+        case "page":
+
+
+            $delete_page = mysqli_query($db,"DELETE FROM `pages` WHERE PageID = '$getid'");
+            $delete_page_comments = mysqli_query($db,"DELETE FROM `comments` WHERE pageID = '$getid'");
+            $delete_pages_votes = mysqli_query($db,"DELETE FROM `votes` WHERE pageID = '$getid'");
+            $delete_pages_pictures = mysqli_query($db,"DELETE FROM `picture_gallery_pages` WHERE pageID = '$getid'");
+
+
+            $msg = "Delete group permissions successful";
+            echo "<script> alert('Group Added Successfully');</script>";
+            $page = "./?page=admin";
+            header("Refresh: 2; URL=\"" . $page . "\"");
+
+            break;
 
     }
 } else {

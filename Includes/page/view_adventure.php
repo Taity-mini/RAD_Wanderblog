e38da4d1a4ab0c9644c05e265892c282a8d3b728
Taity-mini <taitymini@hotmail.co.uk>
2015-12-18 15:14:53 +0000

    Update adventure picture viewing

--- Includes/page/view_adventure.php
+++ Includes/page/view_adventure.php
@@ -228,7 +228,7 @@ if (mysqli_num_rows($pictures) > 0)
         echo "<div id =id='Personal-Highest'";
         echo "<img style='width:350px; height:350px;' src='".$images['filePath']."' /></br>";
         echo "<p>" .$images['photoDesc']. "</p>";
-        echo "</div";
+        echo "</div>";
         echo "</td>";
       }
      echo"</table>";

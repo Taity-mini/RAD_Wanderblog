a6e22b671c910c9713e229152e36334a7707be45
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-18 13:18:01 +0000

    Update index.php

--- index.php
+++ index.php
@@ -234,7 +234,7 @@ $vote1 = mysqli_query($db,"SELECT SUM(vote_Count) as count, pageID FROM `votes` 
 <h2> </h2>
     <div id = "Content-outter">
     <?php
-    while($pictures = mysqli_fetch_assoc($vote1)) {
+    while($pictures = mysqli_fetch_array($vote1)) {
         			$picsID = $pictures['pageID'];
 					$getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID'");
 				    while ($images = mysqli_fetch_array($getPics)) {

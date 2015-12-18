df726e3265f12ae4abb8e9748e412b6778e6e8d8
Taity-mini <taitymini@hotmail.co.uk>
2015-12-18 15:12:57 +0000

    Update adventure picture viewing

--- Includes/page/view_adventure.php
+++ Includes/page/view_adventure.php
@@ -223,8 +223,8 @@ if (mysqli_num_rows($pictures) > 0)
     print "<table border='1' cellspacing='0'>";
      while($images = mysqli_fetch_array($pictures))
       {
-        echo "<trstyle='background-color:#000000;'>";
-        echo "<td id='Personal-Highest'>";
+        echo "<trstyle='background-color:#000000;' id='Personal-Highest'>";
+        echo "<td>";
         echo "<img style='width:350px; height:350px;' src='".$images['filePath']."' /></br>";
         echo "<p>" .$images['photoDesc']. "</p>";
         echo "</td>";

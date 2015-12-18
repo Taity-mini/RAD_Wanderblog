1a8e95fe5e92e5689e70f9bc25c59e932d81e690
DanyalXD <danyal1995@hotmail.co.uk>
2015-12-18 13:40:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -239,7 +239,7 @@ $vote1 = mysqli_query($db,"SELECT SUM(vote_Count) as count, pageID FROM `votes` 
 					$getPics = mysqli_query($db, "SELECT * FROM picture_gallery_pages WHERE pageID = '$picsID'");
 				    while ($images = mysqli_fetch_array($getPics)) {
 
-     					echo "<div id = 'Trending_Picture_Container'><header><h4>". $pictures['photoID']. "yolo"."</h4></header></div>";
+     					echo "<div id = 'Trending_Picture_Container'><header><h4>". $images['filePath']. " yolo"."</h4></header></div>";
 			
      ?>
      					<style>
